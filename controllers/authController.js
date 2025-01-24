6c3c69484858b58aaabc4198006209a0d37ba168
Fedry Alvindra <114293869+fedryalvindra@users.noreply.github.com>
2025-01-24 16:34:32 +0700

    add forget password pug email

--- controllers/authController.js
+++ controllers/authController.js
@@ -197,19 +197,19 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // validateBeforeSave = deactivate all validator that we specify in schema
   await user.save({ validateBeforeSave: false });
 
-  // 3). Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host',
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-
   try {
+    // 3). Send it to user's email
+    const resetURL = `${req.protocol}://${req.get(
+      'host',
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
     // await sendEmail({
     //   email: user.email,
     //   subject: 'Your password reset token (valid for 10min)',
     //   message,
     // });
+
+    await new Email(user, resetURL).sendPasswordReset();
 
     res
       .status(200)
